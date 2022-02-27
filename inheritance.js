1b6ae9f77efdce162875bbb6f2896e26639ebc54
RakibHossainfr1 <rakibhossain.fr@gmail.com>
2022-02-27 11:11:32 +0600

    added inheritance

--- inheritance.js
+++ inheritance.js
@@ -9,16 +9,59 @@ class Madrasah {
     }
 }
 class ClassEight extends Madrasah {
-    
+    vaccice;
+    constructor (name, clases, roll, vaccice){
+    super(name, clases, roll)
+    this.vaccice = vaccice;
+    }
 }
 
 class ClasNine extends Madrasah {
-   
+   vaccice;
+   constructor(name, clases, roll, vaccice){
+       super(name, clases, roll)
+       this.vaccice = vaccice;
+   }
 }
 class ClassTen extends Madrasah {
+    vaccice;
+    constructor(name, clases, roll, vaccice){
+        super(name, clases, roll)
+        this.vaccice = vaccice;
+    }
+}
+const habib = new ClassEight('Habib', 'Eight', 12, 'Complete');
+const asad = new ClasNine('asad', 'Nine', 8, 'No-Complete');
+const arif = new ClassTen('arif', 'Ten', 155, 'Complete')
+// console.log(habib, asad, arif);
+
+
+// Building calculation use maltiple class 
+
+class Building {
+    name = 'Building';
+    room;
+    window;
+    door;
+    constructor(name, room, window, door){
+        this.name = name;
+        this.room = room;
+        this.window = window;
+        this.door =  door;
+    }
+}
+class FourRoomBuil extends Building {
     
 }
-const habib = new ClassEight('Habib', 'Eight', 12);
-const asad = new ClasNine('asad', 'Nine', 8);
-const arif = new ClassTen('arif', 'Ten', 1)
-console.log(habib, asad, arif);
+class SixRoomBuil extends Building {
+
+}
+class NineRoomBuil extends Building {
+
+}
+
+const fourRoom = new FourRoomBuil('Four Room',4, 12, 10);
+const sixRoom = new SixRoomBuil('Six Room',6, 18, 15);
+const nineRoom = new NineRoomBuil('Nine Room',9, 12, 22.5);
+
+console.log(fourRoom, fourRoom, sixRoom, nineRoom);
